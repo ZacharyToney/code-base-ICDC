7c6a1b78543262b2f40dcf3a4ea8111bd4fe4452
Zachary Toney <zach@clearseries.com>
2018-12-06 10:09:20 -0500

    Update generalTest.php

--- tests/generalTest.php
+++ tests/generalTest.php
@@ -5,10 +5,23 @@ include_once('C:\xampp\php\phpunit');
 final class generalTests extends PHPUnit_Framework_TestCase
 {
     /** @test */
-    public function simpleTest()
+    public function simpleTests()
     {
         $bool = true;
-        
         $this->assertTrue($bool);
+
+        $number = 5;
+        $this->assertTrue($number == 5);
+
+        $letter = 'a';
+        $this->assertTrue($letter == 'a');
+        
+        $numberArray = [1,2,3,4];
+        $this->assertTrue($numberArray[0] == 1);
+        $this->assertTrue(count($numberArray) == 4);
+        array_pop($numberArray);
+        $this->assertTrue(count($numberArray) == 3);
     }
+
+
 }
