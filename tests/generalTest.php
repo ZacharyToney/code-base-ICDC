600b5fb49bae72c3687180a183f2233c8c50ed1c
Zachary Toney <zach@clearseries.com>
2018-12-06 10:00:49 -0500

    Update generalTest.php

--- tests/generalTest.php
+++ tests/generalTest.php
@@ -1,1 +1,14 @@
+<?php
+declare(strict_types=1);
 
+include_once('C:\xampp\php\phpunit');
+
+
+final class EmailTest extends PHPUnit_Framework_TestCase
+{
+    public function simpleTest()
+    {
+        $stack = [];
+        $this->assertSame(0, count($stack));
+    }
+}
