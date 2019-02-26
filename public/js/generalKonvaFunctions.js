378f23f5639ff462f4d2997dfcda0eca25512279
Alex Baker <bakerab@sbcglobal.net>
2019-02-26 13:58:25 -0500

    Forgot to add function

--- public/js/generalKonvaFunctions.js
+++ public/js/generalKonvaFunctions.js
@@ -202,6 +202,15 @@ $('#jsonClassRoomStringsFromDatabase').change(function () {
    loadJsonString(valueSelected);
 });
 
+function downloadURI(uri, name) {
+	var link = document.createElement('a');
+	link.download = name;
+	link.href = uri;
+	document.body.appendChild(link);
+	link.click();
+	document.body.removeChild(link);
+	delete link;
+}
 document.getElementById('save').addEventListener(
 	'click',
 	function() {
